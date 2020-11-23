0abd8afd981f241eaac849a0327527b0c5f7a307
derberg <lpgornicki@gmail.com>
2020-11-23 20:46:37 +0100

    chore: add proper logs to workflow so it is clear what action started

--- dist/index.js
+++ dist/index.js
@@ -11668,30 +11668,37 @@ async function run() {
   //TODO for now this action is hardcoded to always get commit id of the first commit on the list
   const commitId = eventPayload.commits[0].id;
   const ignoredRepositories = [repo];
+
+  core.info(`Getting list of modified workflow files from ${commitId} located in ${owner}/${repo}.`);
   const modifiedFiles = await getListModifiedFiles(octokit, commitId, owner, repo);
 
   if (!modifiedFiles.length) 
     return core.info('No changes to workflows were detected.');
 
+  core.info(`Getting list of repositories owned by ${owner} that will get updates.`);
   const reposList = await getReposList(octokit, owner);
 
   for (const {url, name, id} of reposList) {
     if (ignoredRepositories.includes(name)) return;
 
+    core.startGroup(`Started updating ${name} repo`);
     const dir = path.join(process.cwd(), './clones', name);
     await mkdir(dir, {recursive: true});
 
     const branchName = `bot/update-global-workflow-${commitId}`;
     const git = simpleGit({baseDir: dir});
 
+    core.info(`Clonning ${name}.`);
     await clone(url, dir, git);
+    core.info(`Creating branch ${branchName}.`);
     await createBranch(branchName, git);
+    core.info('Copying files...');
     await copyChangedFiles(modifiedFiles, dir);
+    core.info('Pushing changes to remote');
     await push(gitHubKey, owner, url, branchName, 'Update global workflows', git);
 
     const pullRequestUrl = await createPr(octokit, branchName, id);
-
-    console.log(`Entire workflow works like a charm, PR for ${name} is created -> ${pullRequestUrl}`);
+    core.endGroup(`PR for ${name} is created -> ${pullRequestUrl}`);
   }
 }
 
