210b29408a0d4c87ac48bf445fc135b1a1780219
derberg <lpgornicki@gmail.com>
2022-03-21 12:52:39 +0100

    Update git.js

--- dist/index.js
+++ dist/index.js
@@ -7619,7 +7619,7 @@ async function clone(token, remote, dir, git) {
 
   if (core.isDebug()) __webpack_require__(231).enable('simple-git');
   core.info('Fetching all.');
-  await git.fetch({'--all': null});
+  await git.fetch(['--all', '-v']);
 }
 
 async function getBranchesLocal(git) {
